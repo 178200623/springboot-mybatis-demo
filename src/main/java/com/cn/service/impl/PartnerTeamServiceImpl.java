8bf2e495e8ca0f0bf3ff1178d59c43e1760caa63
178200623 <178200623@qq.com>
2018-06-06 09:52:01 +0800

    PartnerTeamService by Wangsd

--- src/main/java/com/cn/service/impl/PartnerTeamServiceImpl.java
+++ src/main/java/com/cn/service/impl/PartnerTeamServiceImpl.java
@@ -5,14 +5,18 @@ import com.cn.service.PartnerTeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class PartnerTeamServiceImpl implements PartnerTeamService {
     @Autowired(required = false)
     private PartnerTeamMapper partnerTeamMapper;
 
+
     @Override
-    public void selelctAllPartner(int id) throws Exception {
+    public List<Map<String, Object>> selelctAllPartner(int id) throws Exception {
 
-
+        return partnerTeamMapper.selectAllPartner(id);
     }
 }
